45c338a6050af818290ff94ea4083769554b42e9
DrunkLeen <reza.df.x@gmail.com>
2023-01-17 04:22:24 +0100

    bug fix

--- main.py
+++ main.py
@@ -5,48 +5,51 @@ from food import Food
 from score import Scoreboard
 from snake import Snake
 
-# Screen Setup
-screen = Screen()
-screen.colormode(255)
-screen.bgcolor((0, 0, 0))
-screen.setup(width=800, height=800)
-screen.title("Severus Snake")
-screen.tracer(0)
 
-# Snake Setup
-snake = Snake()
-food = Food()
-score = Scoreboard()
+if __name__ == '__main__':
 
-# KeyBinding
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.up, "w")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.down, "s")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.left, "a")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.right, "d")
+    # Screen Setup
+    screen = Screen()
+    screen.colormode(255)
+    screen.bgcolor((0, 0, 0))
+    screen.setup(width=800, height=800)
+    screen.title("Severus Snake")
+    screen.tracer(0)
 
-# Main Game Loop
-game_running = True
-while game_running:
-    screen.update()
-    time.sleep(0.1)
-    snake.move_snake()
+    # Snake Setup
+    snake = Snake()
+    food = Food()
+    score = Scoreboard()
 
-    # Check if snake eat the food
-    if snake.snake_head.distance(food) <= 25:
-        food.random_new_food()
-        score.add_score()
-        snake.tail_extend()
+    # KeyBinding
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.right, "d")
 
-    # Check if snake eats itself
-    for segment in snake.segments:
-        if segment == snake.snake_head:
-            pass
-        elif snake.snake_head.distance(segment) <= 10:
-            game_running = False
+    # Main Game Loop
+    game_running = True
+    while game_running:
+        screen.update()
+        time.sleep(0.1)
+        snake.move_snake()
 
-screen.mainloop()
+        # Check if snake eat the food
+        if snake.snake_head.distance(food) <= 25:
+            food.random_new_food()
+            score.add_score()
+            snake.tail_extend()
+
+        # Check if snake eats itself
+        for segment in snake.segments:
+            if segment == snake.snake_head:
+                pass
+            elif snake.snake_head.distance(segment) <= 10:
+                game_running = False
+
+    screen.mainloop()
